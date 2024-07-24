4614e021ff377e8095eb96c1c99212337447c43d
Lucas Souza <lucasp.sdev@gmail.com>
2024-07-24 15:48:01 -0300

    feat: custom popup

--- src/CustomMap.tsx
+++ src/CustomMap.tsx
@@ -3,6 +3,7 @@ import React, { useState, useRef, useEffect } from "react";
 import { MapContainer, ImageOverlay, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import "./CustomMap.css";
 
 // Corrige ícones de marker para não sumirem
 delete (L.Icon.Default.prototype as any)._getIconUrl;
@@ -18,7 +19,30 @@ L.Icon.Default.mergeOptions({
 // Cria ícones personalizados
 const icon1 = new L.Icon({
   iconUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+    "data:image/svg+xml;base64," +
+    btoa(`
+      <svg xmlns="http://www.w3.org/2000/svg" width="98" height="108" viewBox="0 0 98 108" fill="none">
+        <g filter="url(#filter0_d_26489_40997)">
+          <path d="M49 29.5C43.8964 29.5058 39.0034 31.5358 35.3946 35.1446C31.7858 38.7534 29.7558 43.6464 29.75 48.75C29.75 65.2219 47.25 77.6622 47.9959 78.1828C48.2902 78.3889 48.6407 78.4995 49 78.4995C49.3593 78.4995 49.7098 78.3889 50.0041 78.1828C50.75 77.6622 68.25 65.2219 68.25 48.75C68.2442 43.6464 66.2142 38.7534 62.6054 35.1446C58.9966 31.5358 54.1036 29.5058 49 29.5ZM49 41.75C50.3845 41.75 51.7378 42.1605 52.889 42.9297C54.0401 43.6989 54.9373 44.7921 55.4672 46.0712C55.997 47.3503 56.1356 48.7578 55.8655 50.1156C55.5954 51.4735 54.9287 52.7208 53.9497 53.6997C52.9708 54.6787 51.7235 55.3454 50.3656 55.6155C49.0078 55.8856 47.6003 55.747 46.3212 55.2172C45.0421 54.6873 43.9489 53.7901 43.1797 52.639C42.4105 51.4878 42 50.1345 42 48.75C42 46.8935 42.7375 45.113 44.0503 43.8003C45.363 42.4875 47.1435 41.75 49 41.75Z" fill="url(#paint0_linear_26489_40997)"/>
+        </g>
+        <defs>
+          <filter id="filter0_d_26489_40997" x="-8" y="-3" width="114" height="114" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset/>
+            <feGaussianBlur stdDeviation="14.5"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0"/>
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_26489_40997"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_26489_40997" result="shape"/>
+          </filter>
+          <linearGradient id="paint0_linear_26489_40997" x1="49" y1="29.5" x2="49" y2="78.4995" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#46FF90"/>
+            <stop offset="1" stop-color="#2DA75E"/>
+          </linearGradient>
+        </defs>
+      </svg>
+    `),
   iconSize: [32, 32],
   iconAnchor: [16, 32],
   popupAnchor: [0, -32],
@@ -48,6 +72,7 @@ const CustomMap: React.FC = () => {
     [0, 0],
     [1000, 1000],
   ]; // Define as coordenadas dos cantos da imagem
+
   const imageUrl =
     "https://lh3.googleusercontent.com/-qx_UnyJwGHc/YBIqHMKwwbI/AAAAAAAAasA/uVamQSZvyvw4wMfFB44x1LxP-KOcI7VlgCLcBGAsYHQ/s16000/gta-sa-san-andreas-detailed-radar-map-satellite-view.jpg"; // Substitua pela URL da sua imagem
 
@@ -97,7 +122,7 @@ const CustomMap: React.FC = () => {
             position={marker.position}
             icon={index === 0 ? icon1 : index === 1 ? icon2 : icon3}
           >
-            <Popup>
+            <Popup className="custom-popup">
               <div>
                 <h3>{marker.text}</h3>
               </div>
